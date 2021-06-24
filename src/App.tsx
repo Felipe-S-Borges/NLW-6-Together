bdaf12e5dd49dccf7e47e50dcd5537feef7813cb
Felipe <felipelee22@gmail.com>
2021-06-24 14:52:23 -0300

    Dia 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
 
+import { BrowserRouter, Route } from "react-router-dom";
+import { AuthContextProvider } from './contexts/authContext';
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+
+
+
+
+
 
 function App() {
+  
   return (
-    <h1>Hi Lorena</h1>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Route path="/" exact component={Home}/>
+        <Route path="/rooms/new" component={NewRoom}/>
+      </AuthContextProvider>
+    </BrowserRouter>
   );
 }
 
