85ffcfef917a7978da404fc02e74ac33db37bddf
Felipe <felipelee22@gmail.com>
2021-06-25 11:21:54 -0300

    Dia 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { AuthContextProvider } from './contexts/authContext';
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
-
-
-
+import { Room } from "./pages/Room";
 
 
 
@@ -14,8 +12,11 @@ function App() {
   return (
     <BrowserRouter>
       <AuthContextProvider>
-        <Route path="/" exact component={Home}/>
-        <Route path="/rooms/new" component={NewRoom}/>
+        <Switch>
+          <Route path="/" exact component={Home}/>
+          <Route path="/rooms/new" exact component={NewRoom}/>
+          <Route path="/rooms/:id" component={Room}/>
+        </Switch>
       </AuthContextProvider>
     </BrowserRouter>
   );
